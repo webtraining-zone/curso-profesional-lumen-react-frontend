72e5cc4e0c874e0cc007dede955d916829949475
Alex Arriaga <alexzander.arriaga@base22.com>
2018-10-15 18:43:29 -0500

    Reemplazar Link por NavLink para permitir estilos personalizados

--- src/components/common/header/Header.js
+++ src/components/common/header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 class Header extends React.Component {
   render() {
@@ -8,7 +8,8 @@ class Header extends React.Component {
         <header>
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container">
-              <Link className="navbar-brand" to={'/'}>Project Manager</Link>
+              <NavLink className="navbar-brand" to={'/'}>Project
+                Manager</NavLink>
               <button className="navbar-toggler" type="button"
                       data-toggle="collapse" data-target="#navbarRight"
                       aria-controls="navbarRight" aria-expanded="false"
@@ -18,13 +19,15 @@ class Header extends React.Component {
 
               <div className="collapse navbar-collapse" id="navbarRight">
                 <ul className="navbar-nav ml-auto">
-                  <li className="nav-item active">
-                    <Link className="nav-link" to={'/users'}>Users<span
-                        className="sr-only">(current)</span></Link>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to={'/users'}
+                             activeStyle={{color: '#158CBA'}}>Users<span
+                        className="sr-only">(current)</span></NavLink>
                   </li>
 
-                  <li className="nav-item active">
-                    <Link className="nav-link" to={'/projects'}>Projects</Link>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to={'/projects'}
+                             activeStyle={{color: '#158CBA'}}>Projects</NavLink>
                   </li>
 
                 </ul>
